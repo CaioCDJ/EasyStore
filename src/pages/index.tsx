e1c807f6d21bcab17c7d43ecbe9bb8257cdb31b3
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-16 16:43:48 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import NavBar from "./../components/Navbar";
+
+export const Btn = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: black;
+`;
+
+export default function Home(props: any) {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
